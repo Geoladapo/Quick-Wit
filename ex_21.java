73c6f73c00abace9fe8d82eedb16c9ac53406dcd
oladapo <oladapoelijah49@gmail.com>
2022-06-29 13:31:20 +0100

    vowel && consonant

---
+++ ex_21.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex_21 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a letter: ");
+        String str = in.next().trim();
+
+        if (str.length() > 1){
+            System.out.println(str + " is invalid input.");
+        }else {
+            char ch = str.charAt(0);
+            if(Character.isLetter(ch)) {
+                String s = ch + "";
+                String res = "";
+                if ("aeiou".contains(s.toLowerCase(Locale.ROOT))) {
+                    res += "vowel";
+                } else {
+                    res += "consonant";
+                }
+                System.out.println(ch + " is a "+ res);
+            }else {
+                System.out.println(ch + "is invalid input.");
+            }
+        }
+    }
+}
