53216b99db70bb4f9fd65437b54a42463c9ef91e
oladapo <oladapoelijah49@gmail.com>
2022-06-29 10:59:51 +0100

    ASCII code

---
+++ Exercide17.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercide17 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter  an ASCII code: ");
+
+        int userInput = in.nextInt();
+
+       char output = (char) userInput;
+
+        System.out.println("The character for ASCII code "+ userInput + " is " + output);
+
+    }
+}
