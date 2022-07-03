8ca48f4b8749d447ed17613a4ab8add99beefbf7
oladapo <oladapoelijah49@gmail.com>
2022-07-03 23:51:41 +0100

    practice

---
+++ codes/ex_33.java
@@ -0,0 +1,23 @@
+package codes;
+
+public class ex_33 {
+    public static void main(String[] args) {
+        /*
+         (Generate vehicle plate numbers) Assume a vehicle plate number consists of three
+uppercase letters followed by four digits. Write a program to generate a plate
+number.
+         */
+       String plateNumber = "";
+
+       int i = 0;
+       while (i < 3){
+           plateNumber += (char)(65 + Math.random() * (91-65));
+           i++;
+       }
+
+       for (int j = 0; j < 4; j++){
+           plateNumber += (int)(Math.random() * 9);
+       }
+        System.out.println(plateNumber);
+    }
+}
